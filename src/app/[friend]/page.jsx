eed0178b4806038f027eb2f46e32ee6455a01335
bappy958 <itznobita958@gmail.com>
2026-04-25 22:22:43 +0600

    fix

--- src/app/[friend]/page.jsx
+++ src/app/[friend]/page.jsx
@@ -14,7 +14,8 @@ import { notFound } from "next/navigation";
 async function FriendDetails({ params }) {
   const { friend } = await params;
 
-  const response = await fetch("https://nextjs.org/telemetry/friends.json");
+  const response = await fetch("http://localhost:3000/friends.json" , { cache: "no-store" });
+
   const friendsData = await response.json();
   const data = friendsData.find((item) => item.id === parseInt(friend));
 
